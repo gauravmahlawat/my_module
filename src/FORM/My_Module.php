29fe8a4a951725d16ff0ac2dbadbd4665f56ba06
Gaurav <63394460+Gaurav-frontend@users.noreply.github.com>
2020-04-22 19:08:11 +0530

    Update My_Module.php

--- src/FORM/My_Module.php
+++ src/FORM/My_Module.php
@@ -4,49 +4,58 @@
   * Contains \Drupal\MyModule\Form\MyModuleForm
   */
 namespace Drupal\My_Module\Form;
-use Drupal\Core\Database\Database;
-use Drupal\Core\Form\FormBase;
-use Drupal\Core\FormStateInterface;
-/**
-  * Provide an input form.
-  */
-class MyModuleForm extends FormBase {
-  /**
-    * (@inheritdoc)
-    */
+
+use Drupal\Core\Form\Formbase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+use Drupal\Core\Messenger\MessengerTrait;
+
+class My_Moduleform extends FormBase {
   public function getFormId() {
-  return 'My_Module_new_form';
+    return 'My_Module_new_form';
   }
-  /**
-    * (@inheritdoc)
-    */
-  public function buildForm(array $form, FormStateInterface $form_state){
-    $list = array(0 => 'Select' , 1 => 'Article' , 2 => 'Basic page');
-    $form['NODES'] = array(
-      '#title' => t('Enter the number'),
+    
+  public function buildForm(array $form, FormStateInterface $form_state) { 
+    $node_types = \Drupal\node\Entity\NodeType::loadMultiple();
+    $options = [];
+    foreach ($node_types as $node_type) {
+      $options[$node_type->id()] = $node_type->label();
+    }
+    $form['usernodes'] = [
+      '#title' => t('Enter number'),
       '#type' => 'number',
       '#required' => TRUE,
-    );
-    $form['contenttypes'] = array(
+    ];
+	$form['content'] = [
+	  '#title' => t('Select content types'),
 	  '#type' => 'select',
-	  '#title' => t('Select the content types'),
-	  '#options' => $list,
-	 '#required' => TRUE,
-	);
-    $form['submit'] = array(
-      '#type' => 'submit'
+	  '#options' => $options,
+	  '#required' => TRUE,
+	];
+    $form['submit'] = [
+      '#type' => 'submit',
       '#value' => t('Submit'),
-    );
-    $form['nid'] = array(
+    ];
+    $form['nid'] = [
       '#type' => 'hidden',
       '#value' => $nid,
-    );
+    ];
     return $form;
   }
-  /**
-    * (@inheritdoc)
-    */
+ 
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    drupal_set_message(t('Success'));
+    $abc = $form_state->getValue('usernodes');
+	for($i=0; $i<$abc; $i++{
+	  $node = Node::create([
+              'type' => 'Article',
+              'title' => 'My Node',
+              'langcode' => 'en',
+              'uid' => '1',
+              'status' => 1,
+              'field_fields' => array(),
+            )];
+			$node->save(); 
+    }
+	$this->messenger()->addMessage('Success');
   }
 }
